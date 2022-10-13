9cac2300f52e3d6aa950a493d41a4f79c607b9c5
Viktor Gunnarsson <villegunnar@gmail.com>
2022-10-13 15:35:41 +0200

    final push 3

--- Labb2Avancerad V2/Models/Employee.cs
+++ Labb2Avancerad V2/Models/Employee.cs
@@ -9,7 +9,7 @@
         public string? Email { get; set; }
         public string?    PhoneNumber { get; set; }
         public string? Gender { get; set; }
-
+        
         
         public int DepartmentId { get; set; }
         public Department? Department { get; set; }
